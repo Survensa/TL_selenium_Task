c6fc153f0747af2217fbffe2a437441f7f44f6d8
Suraj Seenivasan <seensura23@gmail.com>
2025-04-04 20:01:31 +0530

    Create Chrome.java
    Home Assignment <2> Understanding Data Types and Variables in Java
    Learning Objective:
    The objective of this assignment is to reinforce your understanding of primitive and non-primitive data types in Java and
    how to declare and use variables.
    Expected CompletionTime:
    Best Case:10 minutes Average
    Case:15 minutes
    Assignment Details:
    Create a Java class named Chrome with variables of various data types and values.
    Requirements:
    a) Create a class named Chrome with a main method.
    b) Inside the main method, declare and initialize the following variables:
    1. version of type float with a value of 91.0f
    2. developer of type Stringwith a value of "Google"
    3. isBeta of type booleanwith a value of false
    4. releaseYear of type int with a value of 2008
    5. shortcutKey of type charwith a value of 'C'
    c) Print the values of all variables using System.out.println. d) Run the program and confirm that the variable values
    are printed in the console.
    Hints:
    1. Data Type Selection:Ensure thatyouchoose appropriatedatatypesfor your variables basedonthe nature of the
    data they will hold. For example, use int for whole numbers, float for floating- point numbers, booleanfortrue/false
    values, etc.
    2. Variable Naming: Use meaningful anddescriptive variable namesthat conveythepurpose ofthevariable andshould
    follow the naming convention camelCase
    3. Access Modifiers: Consider using access modifiers like private, public, or protected to controlthe visibility and
    accessibility of your class members(variables and methods).
    Expected Outcome:
    Upon completion of this assignment, you should be able to:
     Understand how to create a package in a Java project.
     Create a Java class with a main method and declare variables of different data types.
     Print variable values using System.out.println.
     Execute a Java program and view the output in the console.

---
+++ Chrome.java
@@ -0,0 +1,17 @@
+public class Chrome {
+    public static void main(String[] args) {
+        // Variable Declaration
+        float ver = 91.0f;
+        String dev = "Google";
+        boolean Beta = false;
+        int release = 2008;
+        char shortcut = 'C';
+
+        // Printing the variables
+        System.out.println("Version: " + ver);
+        System.out.println("Developer: " + dev);
+        System.out.println("Is Beta: " + Beta);
+        System.out.println("Release Year: " + release);
+        System.out.println("Shortcut Key: " + shortcut);
+    }
+}
