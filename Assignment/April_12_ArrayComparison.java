1263fe1cf058eac9f21ab5f041e8b9d79925d4c3
Suraj Seenivasan <seensura23@gmail.com>
2025-04-20 21:20:39 +0530

    Create April_12_ArrayComparison.java
    Assignment Details:
     The assignment requires comparing two arrays and printing matching elements.
     int a[]={3,2,11,4,6,7};
     int b[]=for {1,2,8,4,9,7};
    Requirements: - Declare An array for {3,2,11,4,6,7}; - Declare another array for {1,2,8,4,9,7}; - Declare for loop iterator from 0 to array length - Declare a nested for another array from 0 to array length - Compare both the arrays using a conditional statement. - Print the value from first array when there is a match
     Hints to Solve:  Declare two separate arrays, array1 and array2, to store the given integer values.  Use a nested for loop to iterate over each element in both arrays.  Within the nested loop, compare the elements of array1 with the elements of array2 using an if statement.  If a match is found, print the matching element using System.out.println().

---
+++ Assignment/April_12_ArrayComparison.java
@@ -0,0 +1,14 @@
+public class April_12_ArrayComparison {
+    public static void main(String[] args) {
+        int[] array1 = {3, 2, 11, 4, 6, 7};
+        int[] array2 = {1, 2, 8, 4, 9, 7};
+
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = 0; j < array2.length; j++) {
+                if (array1[i] == array2[j]) {
+                    System.out.println(array1[i]);
+                }
+            }
+        }
+    }
+}
